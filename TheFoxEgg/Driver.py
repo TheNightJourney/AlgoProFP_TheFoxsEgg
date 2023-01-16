9ee04cb2f521a6a06c8d07fc81dc120982b8705d
yoji <106401357+TheNightJourney@users.noreply.github.com>
2023-01-16 13:24:11 +0700

    Add files via upload

---
+++ TheFoxEgg/Driver.py
@@ -0,0 +1,10 @@
+# The Fox's Egg Financial Program
+# Andrew Tanuwijaya Algorithm and Programming Final Project
+
+from window import*
+
+if __name__ == "__main__":
+    app = FoxEggApp()
+    app.MainLoop()
+    
+    
